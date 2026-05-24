f9b4675aadcb9482ddd99eee31189f5a5afef146
xvasgit <erlanggapermana8279@gmail.com>
2026-05-24 16:22:48 +0700

    penyelesaian kasus 5 (adit)

--- TugasExceptionHandling/src/kasus5/InsufficientBalanceException.java
+++ TugasExceptionHandling/src/kasus5/InsufficientBalanceException.java
@@ -1,5 +1,9 @@
 package kasus5;
 
-public class InsufficientBalanceException {
-    
+// Mewarisi Exception agar menjadi Checked Exception
+public class InsufficientBalanceException extends Exception {
+    // Constructor untuk menerima pesan error kustom
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
 }
--- TugasExceptionHandling/src/kasus5/MainBank.java
+++ TugasExceptionHandling/src/kasus5/MainBank.java
@@ -1,5 +1,33 @@
 package kasus5;
 
 public class MainBank {
-    
+    private static double saldo = 200000;//saldo awal, static agar dapat dibaca dalam class ini
+    public static void tarikUang(double jumlah) throws InsufficientBalanceException {//pass responsibility exception ini ke class InsufficientBalanceException
+        if (jumlah > saldo) {
+            //throw (buat objek baru) custom exception InsufficientBalanceException jika saldo tidak cukup
+            throw new InsufficientBalanceException("Saldo Anda tidak mencukupi untuk melakukan penarikan sebesar Rp " + jumlah);
+        }
+        
+        //hanya akan berjalan jika saldo cukup
+        saldo -= jumlah;
+    }
+
+    //main class nya
+    public static void main(String[] args){
+        double jumlahTarik = 500000; //jumlah yang ingin ditarik
+
+        System.out.println("Saldo awal Anda: Rp " + saldo);
+        System.out.println("Mencoba menarik: Rp " + jumlahTarik);
+
+        try {//try nya maggil method yang berpotensi throw exception
+            tarikUang(jumlahTarik);
+            System.out.println("Penarikan berhasil! Sisa saldo: Rp " + saldo);//jika tidak terkena exception(aman), maka ini ikut di-run
+        } catch (InsufficientBalanceException e){//catch nya baru menangkap dan menangani custom exception yang terjadi
+            System.out.println("\n[TRANSAKSI GAGAL]");
+            System.out.println("Pesan Error: " + e.getMessage());//method getMessage berasal dari class Throwable, superclass dari Exception (superclass InsufficientBalanceException)
+        }
+    }
+
+
+
 }
