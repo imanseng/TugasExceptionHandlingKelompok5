0213f2cd84b2d1be9f539b93b7e9d9f4d0609c10
Sennn306 <adrenalinsyahrobby30@gmail.com>
2026-05-25 05:28:00 +0700

    Penyelesaian kasus 4

--- TugasExceptionHandling/src/kasus4/MainPeringkat.java
+++ TugasExceptionHandling/src/kasus4/MainPeringkat.java
@@ -1,5 +1,16 @@
 package kasus4;
 
 public class MainPeringkat {
-    
+    public static void main(String[] args) {
+        String[] mahasiswa = {"Agus", "Udin", "Ujang", "Dimas", "Ucup"};
+        int peringkatDicari = 10;
+
+        try {
+            String nama = mahasiswa[peringkatDicari - 1];
+            System.out.println("Mahasiswa peringkat ke-" + peringkatDicari + ": " + nama);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Peringkat ke-" + peringkatDicari + " tidak ditemukan.");
+            System.out.println("Data peringkat hanya tersedia dari 1 hingga " + mahasiswa.length + ".");
+        }
+    }
 }
